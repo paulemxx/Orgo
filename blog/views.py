2d6bd4182dd46aa2cc4c2b2111ae894d79fc1ca2
paulemxx <51763335+paulemxx@users.noreply.github.com>
2020-10-19 18:36:22 +0000

    views/blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.http.response import HttpResponse, JsonResponse
+from django.http.request import HttpRequest
+from . import models
+from siteConfig.datamanager import mergeData
 
 # Create your views here.
+
+def blog(request: HttpRequest) -> HttpResponse:
+    data = {
+
+        'categories': models.Categorie.objects.filter(status=True).order_by('-date_add'),
+
+
+        'article': models.Article.objects.filter(status=True)[:4],
+        'articles': models.Article.objects.filter(status=True),
+        'tags': models.Tag.objects.filter(status=True)[:5],
+
+    }
+    return render(request, 'pages/blog/blog.html', mergeData(request, data))
+
+
+def single_blog(request: HttpRequest, titre_slug: str) -> HttpResponse:
+    data = {
+
+        'categories': models.Categorie.objects.filter(status=True).order_by('-date_add'),
+
+        'singles': models.Article.objects.filter(titre_slug=titre_slug)[:1],
+        'tags': models.Tag.objects.filter(status=True)[:5],
+        'commentaires': models.Commentaire.objects.filter(status=True).order_by('-date_add')[:6],
+
+    }
+
+    return render(request, 'pages/blog/single_blog.html', mergeData(request, data))
+
+
+
+
+
