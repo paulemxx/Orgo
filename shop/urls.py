2e60d11f4b366c2aba019ee2b685b4e78ea96766
GPamichia <gnogan.paul@gmail.com>
2020-10-19 21:16:08 +0000

    urls/shop

---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('', views.shop, name='index'),
+    path('shop/<slug:titre_slug>', views.product, name='product'),
+
+]
