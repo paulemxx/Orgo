6ff2b72d4ebd2210c708426d23a649bef845e66a
GPamichia <gnogan.paul@gmail.com>
2020-10-19 21:28:46 +0000

    urls/views shop

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http.request import HttpRequest
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from . import models
+from siteConfig.datamanager import mergeData
+
+from django.contrib.messages import constants as messages
+
 
 # Create your views here.
+
+def shop(request: HttpRequest) -> HttpResponse:
+    data = {
+
+
+        'prod': models.Produit.objects.filter(status=True).order_by('-date_add')[:3]
+
+    }
+    return render(request, 'pages/shop/shop.html', mergeData(request, data))
+
+
+def product(request: HttpRequest, titre_slug: str) -> HttpResponse:
+    data = {
+        'single': models.Produit.objects.filter(status=True, titre_slug=titre_slug)[:1],
+        'prod': models.Produit.objects.filter(status=True).order_by('-date_add')[:4],
+
+    }
+    return render(request, 'pages/shop/product.html', data)
+
+
+
