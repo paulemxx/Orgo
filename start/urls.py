efbb40b4c65eebb23241d708ce71909d0ebea28d
GPamichia <gnogan.paul@gmail.com>
2020-10-19 21:42:34 +0000

    urls/start

---
+++ start/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = 'start'
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('gallery', views.gallery, name='gallery'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
+    path('faq', views.faq, name='faq'),
+    path('team', views.contact, name='team'),
+]
