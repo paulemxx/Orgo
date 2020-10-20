35fb9effce9e4443106ed41957f4d43a49956612
GPamichia <gnogan.paul@gmail.com>
2020-10-20 10:51:03 +0000

    add colorfield installed apps

--- projet/settings.py
+++ projet/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
