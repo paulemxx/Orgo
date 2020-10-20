96b4a5a224ac9b79d5656a4ecbb6371516a51552
GPamichia <gnogan.paul@gmail.com>
2020-10-20 11:39:21 +0000

    remplissage views accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.http.response import HttpResponse, JsonResponse
+from django.http.request import HttpRequest
+from . import models
+from siteConfig.datamanager import mergeData
 
 # Create your views here.
+
+def register(request: HttpRequest) -> HttpResponse:
+    data = {
+
+
+    }
+    return render(request, 'pages/accounts/register.html', mergeData(request, data))
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    data = {
+
+    }
+    return render(request, 'pages/accounts/login.html', mergeData(request, data))
