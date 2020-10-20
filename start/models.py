e18899afd9d6d7a0ebbfb86e20bed46e9d9cd8e3
GPamichia <gnogan.paul@gmail.com>
2020-10-20 00:10:36 +0000

    recyclycage des voleurs

--- start/models.py
+++ start/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from tinymce import HTMLField
+import hashlib
+from django.utils.text import slugify
+from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
+from shop.models import Produit
+from blog.models import Article
 
-# Create your models here.
+#TODO : About us
+
+class Taffich(models.Model):
+
+    titre = models.CharField(max_length=255, null=True, blank=True)
+    description = HTMLField('shop/descripion')
+    cover = models.ImageField(upload_to='blog')
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Advantage"
+        verbose_name_plural = "Advantages"
+
+    def __str__(self) -> str:
+        return str(self.titre)
