14bfeabc597d0ccc357899dc50fea8d05d64245d
GPamichia <gnogan.paul@gmail.com>
2020-10-20 12:42:31 +0000

    debut views register

--- accounts/views.py
+++ accounts/views.py
@@ -4,14 +4,27 @@ from django.http.response import HttpResponse, JsonResponse
 from django.http.request import HttpRequest
 from . import models
 from siteConfig.datamanager import mergeData
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 
+from accounts.forms import RegisterForm
 
 # Create your views here.
 
 def register(request: HttpRequest) -> HttpResponse:
-    data = {
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        data = {
 
-    }
+        }
     return render(request, 'pages/accounts/register.html', mergeData(request, data))
 
 
