93f3f3db9143f0eec9b8396b177bbbc5340b5cbb
paulemxx <51763335+paulemxx@users.noreply.github.com>
2020-10-20 02:03:15 +0000

    models contact siteConfig

--- siteConfig/models.py
+++ siteConfig/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from tinymce import HTMLField
+import hashlib
+from django.utils.text import slugify
+from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
+from shop.models import Produit
+from blog.models import Article
 
-# Create your models here.
+
+
+# TODO : Contact
+
+class Adresse(models.Model):
+    titre = models.CharField(max_length=255, null=True, blank=True)
+    adress = models.CharField(max_length=255, null=True, blank=True)
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Adresse"
+        verbose_name_plural = "Adresses"
+
+
+class Mail(models.Model):
+    titre = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Mail"
+        verbose_name_plural = "Mails"
+
+
+class Call(models.Model):
+    titre = models.CharField(max_length=255, null=True, blank=True)
+    tel = models.CharField(max_length=255, null=True, blank=True)
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Call"
+        verbose_name_plural = "Calls"
