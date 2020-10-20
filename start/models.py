7077cf1ea494eb2b4ffe0cd7baec91fe92590b1f
paulemxx <51763335+paulemxx@users.noreply.github.com>
2020-10-20 01:56:51 +0000

    models contact ok

--- start/models.py
+++ start/models.py
@@ -216,3 +216,44 @@ class Services(models.Model):
     def __str__(self) -> str:
         return str(self.titre)
 
+#TODO : Contact
+
+class Adresse(models.Model):
+
+    titre = models.CharField(max_length=255, null=True, blank=True)
+    adress = models.CharField(max_length=255, null=True, blank=True)
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Adresse"
+        verbose_name_plural = "Adresses"
+
+class Mail(models.Model):
+
+    titre = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Mail"
+        verbose_name_plural = "Mails"
+
+
+class Call(models.Model):
+
+    titre = models.CharField(max_length=255, null=True, blank=True)
+    tel = models.CharField(max_length=255, null=True, blank=True)
+
+    status = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Call"
+        verbose_name_plural = "Calls"
