110b4bed2992e64e1a5e342ca0d675e5f7d64fbf
paulemxx <51763335+paulemxx@users.noreply.github.com>
2020-10-20 09:22:03 +0000

    importation de model de start dans siteConfig

--- siteConfig/models.py
+++ siteConfig/models.py
@@ -6,47 +6,11 @@ from django.contrib.auth.models import User
 from django.db.models.query import QuerySet
 from shop.models import Produit
 from blog.models import Article
+from start.models import Call
+from start.models import Mail
+from start.models import Adresse
+# TODO : Contact : Importation de start
 
-
-# TODO : Contact
-
-class Adresse(models.Model):
-    titre = models.CharField(max_length=255, null=True, blank=True)
-    adress = models.CharField(max_length=255, null=True, blank=True)
-
-    statut = models.BooleanField(default=True)
-    date_add = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Adresse"
-        verbose_name_plural = "Adresses"
-
-
-class Mail(models.Model):
-    titre = models.CharField(max_length=255, null=True, blank=True)
-    email = models.EmailField(max_length=255, null=True, blank=True)
-
-    statut = models.BooleanField(default=True)
-    date_add = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Mail"
-        verbose_name_plural = "Mails"
-
-
-class Call(models.Model):
-    titre = models.CharField(max_length=255, null=True, blank=True)
-    tel = models.CharField(max_length=255, null=True, blank=True)
-
-    statut = models.BooleanField(default=True)
-    date_add = models.DateTimeField(auto_now_add=True)
-    date_update = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "Call"
-        verbose_name_plural = "Calls"
 
 
 # TODO : Liens
