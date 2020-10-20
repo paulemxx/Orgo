f35b28112ea823a03f89dfdcabf32de68b8d7aaf
GPamichia <gnogan.paul@gmail.com>
2020-10-20 13:20:48 +0000

    models profile receive aaccounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30, required=False, help_text='first_name')
+    last_name = forms.CharField(max_length=30, required=False, help_text='last_name')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
