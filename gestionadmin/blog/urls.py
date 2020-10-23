332454f51d7086b98d97123818476c5b5b3043e3
GPamichia <gnogan.paul@gmail.com>
2020-10-23 23:49:23 +0000

    python package blog & shop

---
+++ gestionadmin/blog/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+
+]
